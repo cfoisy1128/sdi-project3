bf2649a80b4d465b7de0bbbd07e381d66743b94e
Christine Foisy <christinefoisy@new-host-2.home>
2012-06-12 16:55:27 -0400

    Adding original files to repository

---
+++ Foisy_Christine_assignment3.js
@@ -0,0 +1,95 @@
+// Christine Foisy
+// SDI, Week 3 Assignment
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//var needHelp = true;
+
+
+//if (needHelp = true) {
+//		console.log ("What can we help you with");
+//		}
+		
+//var wantToHelp = true;   
+//if (wantToHelp === true) {
+		//console.log ("How would you like to help.");
+		//	} 
+
+
+
+
+//var typesOfHelp = ["Food", "Rental Assistance", "Housing", "Shelter"],
+//var	whoCanHelp = ["Sue", "Bill", "Monica", "Marie"],
+//var extension = ["230", "211", "205", "236" ]	
+	
+
+       // var typesOfHelp = function(typesOfHelp) {
+         //   var whoCanHelp = typesOfHelp[whoCanHelp],
+            
+           //       console.log(whoCanHelp[0] + " Can offer information on housing " );
+                  
+            
+            //for (var minutes = 0; minutes < minutesThisTank; minutes += 5) {
+                    //var minutesRemain = minutesThisTank - minutes;
+                  //  console.log("We have " + minutesRemain + " minutes left. " + minutes + " minutes have past.");
+           // }
+            
+            
+            
+           // console.log("We finished with the " + tankName + ".");};
+        
+        
+
+        //for (var tankNumber = 0; tankNumber < tankNames.length; tankNumber++) {
+          //  visitTank(tankNumber); };
+    
+
+
+
+
+
