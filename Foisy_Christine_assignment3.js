b1956703f911235e7c101152b5370fed921b3d62
Christine Foisy <christinefoisy@new-host-2.home>
2012-06-14 18:30:55 -0400

    Updating changes to Github from 6/14.

--- Foisy_Christine_assignment3.js
+++ Foisy_Christine_assignment3.js
@@ -1,113 +1,127 @@
 // Christine Foisy
 // SDI, Week 3 Assignment
+// Build a Deck
+
+var deck1
+deck1 = "12x12 moderate deck"
+
+var deck2
+deck2 = "18x12 large deck"
+
+//object and json data (only have properties) and method/function
+
+var deck1 = {
+	"name" : "backyard deck option 1",
+	"width" : 12, 
+	"length" : 12,
+	"area" : function(){
+		totalArea = this.width * this.length;
+		console.log("Total area of deck option 1 " + totalArea);
+	}
+};
+
+deck1.area();
 
 
+//return within a function
 
+var deck2 = {
+	"name" : "backyard deck option 2",
+	"width" : 18, 
+	"length" : 12,
+	"getArea" : function(){
+		newArea = this.width * this.length;
+		return newArea;
+	}
 
-//object and json data (only have properties)
+};
 
-var item = {
-	"itemNumber" : "01234",
-	"name" : "shirt",
-	"color" : "red",
-	"size" : "large",
+console.log("Total is the return of the area of deck option 2 " + deck2.getArea());
 	
+//Conditional
+var doesItCostLessThen1200 = true;
+	
+if (doesItCostLessThen1200 === true) {
+ 	console.log("We can build the bigger deck!)";
+else (doesItCostLessThen1200 === false)
+	console.log("we will have to build the smaller deck.)";
 };
 
 
+//Method Accessor, Array, 
 
-var checkOut = {
-	"name" : "Jane Smith", 
-	"address" : "123 Main Ave",
-	"city, state zip" : "Richmond, RI 02892",
-	"phone" : "401-444-1234",
-	"email" : "jane@yahoo.com",
-	
+var contractorName=new Array();
+contractorName[0]="Joe";
+contractorName[1]="Dave";
+contractorName[2]="Bill";
+
+get contractorName(2)
+{
+return contractorName
 }
 
+console.log(contractorName[2];
 
+//FOR LOOP
+for (var contractorQuote = 1400; contractorQuote < 1400; contractorQuote--) {};
+	console.log(contractorQuote + "is above our budget".);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//var needHelp = true;
-
-
-//if (needHelp = true) {
-//		console.log ("What can we help you with");
-//		}
-		
-//var wantToHelp = true;   
-//if (wantToHelp === true) {
-		//console.log ("How would you like to help.");
-		//	} 
-
-
-
-
-//var typesOfHelp = ["Food", "Rental Assistance", "Housing", "Shelter"],
-//var	whoCanHelp = ["Sue", "Bill", "Monica", "Marie"],
-//var extension = ["230", "211", "205", "236" ]	
+};
 	
+console.log ("below our budger. Let's hire them.);
 
-       // var typesOfHelp = function(typesOfHelp) {
-         //   var whoCanHelp = typesOfHelp[whoCanHelp],
-            
-           //       console.log(whoCanHelp[0] + " Can offer information on housing " );
-                  
-            
-            //for (var minutes = 0; minutes < minutesThisTank; minutes += 5) {
-                    //var minutesRemain = minutesThisTank - minutes;
-                  //  console.log("We have " + minutesRemain + " minutes left. " + minutes + " minutes have past.");
-           // }
-            
-            
-            
-           // console.log("We finished with the " + tankName + ".");};
-        
-        
 
-        //for (var tankNumber = 0; tankNumber < tankNames.length; tankNumber++) {
-          //  visitTank(tankNumber); };
-    
+
+
+
+//RETURN VALUES
+
+var didcontractorcallontime = function(ontime) {
+	var currentHour = (new Date()).gethour(),
+		timeofday
+	if (currenthour <12) {timeOfDay = "Contractor called on time!";}
+	else if (currentHour <17) {timeOfDay="Contractor is running late." ; }
+	else {timeOfDay = "Contrator is very late, should not hire him"; }
+	console.log(timeofday + name);
+};
+
+
+//METHOD MUTATOR, WHILE LOOP, MATH, OUTPUT
+
+function contractorName(name) {
+	var contractorName = name;
+	
+	this.getname = function() {
+		return name;
+	};
+	
+	this.setName= function(name {
+	name = name;
+	};
+}
+
+var startDate = 1;
+while (sartDate < 7) {};
+	console.log("Day" + startDate + ", contractor did not start deck today".);
+	startDate++;
+};
+	
+console.log ("Time to fire the contractor");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
